52342338622dbe76ecca73f9c8aeba778a2d583e
ahmed medhat <am510836@gmal.com>
2024-10-26 11:22:00 +0300

    Add about componet

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent , AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
