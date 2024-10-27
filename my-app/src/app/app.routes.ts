748d496549978142d77c9a0909e4845dffaa4002
ahmed medhat <am510836@gmal.com>
2024-10-27 14:25:22 +0300

    add router to the components and creating new branch Route

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+    {path : '' , redirectTo : 'home', pathMatch : 'full'},
+    {path : 'home', component: HomeComponent},
+    {path :'blog' , component : BlogComponent},
+    {path :'about' , component : AboutComponent},
+    {path : '**' , component : NotFoundComponent}
+
+];
--- my-app/src/app/navbar/navbar.component.ts
+++ my-app/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
