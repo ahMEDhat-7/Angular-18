2ca5af1050b97a43b7cc141c09d5c6759823043c
ahmed medhat <am510836@gmal.com>
2024-10-29 07:04:29 +0300

    Data Transfer from component to another component

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ParentComponent } from '../parent/parent.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ParentComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
